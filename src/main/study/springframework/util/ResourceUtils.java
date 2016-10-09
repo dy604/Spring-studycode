5ad7fbd597e623235cae512b18c1ac6410492f61
duanyue <503982643@qq.com>
2016-10-09 23:25:50 +0800

    ResourceUtils完成

--- src/main/study/springframework/util/ResourceUtils.java
+++ src/main/study/springframework/util/ResourceUtils.java
@@ -1,8 +1,8 @@
 package study.springframework.util;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 
 /**
  * Created by pc on 2016/10/8.
@@ -45,6 +45,126 @@ public abstract class ResourceUtils {
     }
 
     public static URL getURL(String resourceLocation) throws FileNotFoundException {
-        Assert.notNull()
+        Assert.notNull(resourceLocation, "Resource location must not be null");
+        if (resourceLocation.startsWith(CLASSPATH_URL_PREFIX)) {
+            String path = resourceLocation.substring(CLASSPATH_URL_PREFIX.length());
+            URL url = ClassUtils.getDefaultClassLoader().getResource(path);
+            if (url == null) {
+                String description = "class path resource [" + path + "]";
+                throw new FileNotFoundException(
+                        description + " cannot be resolved to URL because it does not exist");
+            }
+            return url;
+        }
+        try {
+            return new URL(resourceLocation); //以URL尝试
+        } catch (MalformedURLException ex) {
+            //不是URL，则当做文件路径
+            try {
+                return new File(resourceLocation).toURI().toURL();
+            } catch (MalformedURLException ex2) {
+                throw new FileNotFoundException("Resource location [" + resourceLocation +
+                    "] is neither a URL not a well-formed file path");
+            }
+        }
+    }
+
+    public static File getFile(String resourceLocation) throws FileNotFoundException {
+        Assert.notNull(resourceLocation, "Resource location must not be null");
+        if (resourceLocation.startsWith(CLASSPATH_URL_PREFIX)) {
+            String path = resourceLocation.substring(CLASSPATH_URL_PREFIX.length());
+            String description = "class path resource [" + path + "]";
+            URL url = ClassUtils.getDefaultClassLoader().getResource(path);
+            if (url == null) {
+                throw new FileNotFoundException(
+                    description + " cannot be resolved to absolute file path " +
+                    "because it does not reside int the file system");
+            }
+            return getFile(url, description);
+        }
+        try {
+            return getFile(new URL(resourceLocation));
+        } catch (MalformedURLException ex) {
+            return new File(resourceLocation);
+        }
+    }
+
+    public static File getFile(URL resourceUrl) throws FileNotFoundException {
+        return getFile(resourceUrl, "URL");
+    }
+
+    public static File getFile(URL resourceUrl, String description) throws FileNotFoundException {
+        Assert.notNull(resourceUrl, "Resource URL must not be null");
+        if (!URL_PROTOCOL_FILE.equals(resourceUrl.getProtocol())) {
+            throw new FileNotFoundException(
+                description + " cannot be resolved to absolute file path " +
+                "because it does not reside in the file system: " + resourceUrl);
+        }
+        try {
+            return new File(toURI(resourceUrl).getSchemeSpecificPart());
+        } catch (URISyntaxException ex) {
+            return new File(resourceUrl.getFile());
+        }
+    }
+
+    public static File getFile(URI resourceUri) throws FileNotFoundException {
+        return getFile(resourceUri, "URI");
+    }
+
+    public static File getFile(URI resourceUri, String description) throws FileNotFoundException {
+        Assert.notNull(resourceUri, "Resource URI must not be null");
+        if (!URL_PROTOCOL_FILE.equals(resourceUri.getScheme())) {
+            throw new FileNotFoundException(
+                    description + " cannot be resolved to absolute file path " +
+                    "because it does not reside in the file system: " + resourceUri);
+        }
+        return new File(resourceUri.getSchemeSpecificPart());
+    }
+
+    public static boolean isFileURL(URL url) {
+        String protocol = url.getProtocol();
+        return (URL_PROTOCOL_FILE.equals(protocol) || protocol.startsWith(URL_PROTOCOL_VFS));
+    }
+
+    public static boolean isJarURL(URL url) {
+        String protocol = url.getProtocol();
+        return (URL_PROTOCOL_JAR.equals(protocol) ||
+                URL_PROTOCOL_ZIP.equals(protocol) ||
+                URL_PROTOCOL_WSJAR.equals(protocol) ||
+                (URL_PROTOCOL_CODE_SOURCE.equals(protocol) && url.getPath().contains(JAR_URL_SEPARATOR)));
+    }
+
+    public static URL extractJarFileURL(URL jarUrl) throws MalformedURLException {
+        String urlFile = jarUrl.getFile();
+        int separatorIndex = urlFile.indexOf(JAR_URL_SEPARATOR);
+        if (separatorIndex != -1) {
+            String jarFile = urlFile.substring(0, separatorIndex);
+            try {
+                return new URL(jarFile);
+            }
+            catch (MalformedURLException ex) {
+                // Probably no protocol in original jar URL, like "jar:C:/mypath/myjar.jar".
+                // This usually indicates that the jar file resides in the file system.
+                if (!jarFile.startsWith("/")) {
+                    jarFile = "/" + jarFile;
+                }
+                return new URL(FILE_URL_PREFIX + jarFile);
+            }
+        }
+        else {
+            return jarUrl;
+        }
+    }
+
+    public static URI toURI(URL url) throws URISyntaxException {
+        return toURI(url.toString());
+    }
+
+    public static URI toURI(String location) throws URISyntaxException {
+        return new URI(StringUtils.replace(location, " ", "%20"));
+    }
+
+    public static void useCachesIfNecessary(URLConnection con) {
+        con.setUseCaches(con.getClass().getSimpleName().startsWith("JNLP"));
     }
 }
