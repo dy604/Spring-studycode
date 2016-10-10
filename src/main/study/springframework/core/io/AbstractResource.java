0719e2b6b86b64611c2451ed286666ae8a83bb10
duanyue <503982643@qq.com>
2016-10-10 14:40:27 +0800

    完成

--- src/main/study/springframework/core/io/AbstractResource.java
+++ src/main/study/springframework/core/io/AbstractResource.java
@@ -1,6 +1,7 @@
 package study.springframework.core.io;
 
 import study.springframework.core.NestedIOException;
+import study.springframework.util.Assert;
 import study.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -16,7 +17,6 @@ import java.net.URL;
  */
 public abstract class AbstractResource implements Resource {
 
-    @Override
     public boolean exists() {
         try {
             return getFile().exists();
@@ -31,22 +31,18 @@ public abstract class AbstractResource implements Resource {
         }
     }
 
-    @Override
     public boolean isReadable() {
         return true;
     }
 
-    @Override
     public boolean isOpen() {
         return false;
     }
 
-    @Override
     public URL getURL() throws IOException {
         throw new FileNotFoundException(getDescription() + " cannot be resolved to URL");
     }
 
-    @Override
     public URI getURI() throws IOException {
         URL url = getURL();
         try {
@@ -56,28 +52,63 @@ public abstract class AbstractResource implements Resource {
         }
     }
 
-    @Override
     public File getFile() throws IOException {
         throw new FileNotFoundException(getDescription() + " cannot be resolved to absolute file path");
     }
 
-    @Override
     public long contentLength() throws IOException {
-        return 0;
+        InputStream is = this.getInputStream();
+        Assert.state(is != null, "resource input stream must not be null");
+        try {
+            long size = 0;
+            byte[] buf = new byte[255];
+            int read;
+            while ((read = is.read(buf)) != -1) {
+                size += read;
+            }
+            return size;
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ex) {
+
+            }
+        }
     }
 
-    @Override
     public long lastModified() throws IOException {
-        return 0;
+        long lastModified = getFileForLastModifiedCheck().lastModified();
+        if (lastModified == 0L) {
+            throw new FileNotFoundException(getDescription() + " cannot be resolved in the file system for resolving its last-modified timestamp");
+        }
+        return lastModified;
     }
 
-    @Override
+    protected File getFileForLastModifiedCheck() throws IOException {
+        return getFile();
+    }
+
     public Resource createRelative(String relativePath) throws IOException {
-        return null;
+        throw new FileNotFoundException("Cannot create a relative resource for " + getDescription());
     }
 
-    @Override
     public String getFilename() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return getDescription();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj == this || (
+                obj instanceof Resource && ((Resource) obj).getDescription().equals(getDescription())));
+    }
+
+    @Override
+    public int hashCode() {
+        return getDescription().hashCode();
     }
 }
